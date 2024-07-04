2f563c707e6a920f6223437f654a98d5fc6b16d7
ToufiqAnim <toufiq9610@gmail.com>
2024-07-05 03:14:43 +0600

    order service done

---
+++ src/app/modules/order/order.service.ts
@@ -0,0 +1,39 @@
+import { ProductModel } from "../product/product.model";
+import { IOrder } from "./order.intrerface";
+import { OrderModel } from "./order.model";
+
+const CreateOrderIntoDB = async (data: IOrder) => {
+  const { productId, quantity } = data;
+
+  const productData = await ProductModel.findById(productId);
+
+  if (!productData) {
+    return { message: "Order not found" };
+  }
+  const productQuantity = productData.inventory.quantity - quantity;
+  if (productQuantity < 0)
+    return { message: "Insufficient quantity available in inventory" };
+
+  if (productQuantity === 0) {
+    await ProductModel.findByIdAndUpdate(productId, {
+      "inventory.inStock": false,
+    });
+  }
+  await ProductModel.findByIdAndUpdate(productId, {
+    "inventory.quantity": productQuantity,
+  });
+  const result = await OrderModel.create(data);
+  return result;
+};
+
+const GetAllOrdersFromDB = async (email: string) => {
+  if (email) {
+    return await OrderModel.find({ email: email });
+  }
+  const result = await OrderModel.find();
+  return result;
+};
+export const OrderService = {
+  CreateOrderIntoDB,
+  GetAllOrdersFromDB,
+};
---
+++ src/app/modules/order/order.validation.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const createOrderValidationSchema = z.object({
+  email: z.string().email(),
+  productId: z.string(),
+  price: z.number().min(0),
+  quantity: z.number().min(1),
+});
+
+export const OrderValidation = {
+  createOrderValidationSchema,
+};
